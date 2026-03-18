1e1116ad0c5bd5c1b6a0179db5598e9de1989820
Sakai Newman <sakai.newman@ascotgroup.com>
2026-03-18 15:56:05 -0300

    Feat: Completed Delete function

--- cmd/delete_task.go
+++ cmd/delete_task.go
@@ -1,4 +1,49 @@
 package cmd
 
+import (
+	"flag"
+	"fmt"
+	"os"
+	"slices"
+
+	"github.com/sakaicodes/tasktracker/models"
+)
+
 func DeleteTask(args []string) {
+	fs := flag.NewFlagSet("delete", flag.ExitOnError)
+	id := fs.Int("id", 0, "ID of the task to delete")
+	fs.Parse(args)
+
+	// Check if ID is provided
+	if *id == 0 {
+		fmt.Println("error: --id is required")
+		os.Exit(1)
+	}
+
+	// Loads existing tasks
+	tasks, err := models.LoadTasks()
+	if err != nil {
+		fmt.Println("error:", err)
+		os.Exit(1)
+	}
+
+	// Deletes the task with the specified ID
+	updatedTasks := slices.DeleteFunc(tasks, func(t models.Task) bool {
+		return t.ID == *id
+	})
+
+	// If the length of updatedTasks is the same as tasks, it means the task with the specified ID was not found
+	if len(updatedTasks) == len(tasks) {
+		fmt.Println("error: task with ID", *id, "not found")
+		os.Exit(1)
+	}
+
+	// Saves the updated task list back to the file
+	err = models.SaveTasks(updatedTasks)
+	if err != nil {
+		fmt.Println("error:", err)
+		os.Exit(1)
+	}
+	fmt.Println("Task with ID", *id, "deleted successfully.")
+
 }
